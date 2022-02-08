067fb630c82cedf3c01bea09ba6fd9b04fd7ce57
CatiaGomes82 <cgo@codehousegroup.com>
2022-02-08 09:58:44 +0000

    update references to fix build

--- components/LogIn/index.js
+++ components/LogIn/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import settings from "../../settings";
 
 const LogIn = () => {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Login from '@components/Login';
+import Login from '../components/LogIn';
 
 const Home = () => {
   return (
